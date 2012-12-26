97f144f4b321d080535fe0bd124620b8307a8434
Paul Rayner <paul@virtual-genius.com>
2012-12-26 12:08:45 -0700

    All tests passing

--- features/step_definitions/bulkrename_steps.rb
+++ features/step_definitions/bulkrename_steps.rb
@@ -1,7 +1,5 @@
 Then /^the correct usage message should be displayed:$/ do |usage_message|
   usage_message = 
-  %Q{USAGE: bulkrename <folder name> <find_extension> <replace_extension>
-some more text here
-}
+  %Q{USAGE: bulkrename <folder name> <find_extension> <replace_extension>}
   assert_partial_output(usage_message, all_output)
 end
